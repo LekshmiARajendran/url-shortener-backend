ba67feb9dabc8831b793e47f98cc8b9c75b4f585
Lekshmi A Rajendran <lekshmirajendran408@gmail.com>
2025-07-26 00:34:32 +0200

    Initial commit: Spring Boot Kotlin project with PostgreSQL and Docker setup

---
+++ src/main/kotlin/com/dkb/urlshortener/HealthController.kt
@@ -0,0 +1,11 @@
+package com.dkb.urlshortener
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+@RestController
+class HealthController {
+    @GetMapping("/health")
+    fun healthCheck(): String {
+        return "Application is running!"
+    }
+}
